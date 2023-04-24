fd2493ab77d4fc8c03c7ec99acdf9d9943ed94a8
kaj <40004347+KAJdev@users.noreply.github.com>
2023-04-23 23:11:02 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import aiohttp
 
 app = Sanic(__name__)
 
+async def chunked_response(response, chunk_size=4096):
+    async for chunk in response.content.iter_chunked(chunk_size):
+        yield chunk
+
 async def forward_request(req: Request, path: str):
     forward = req.headers.get("X-Forwarded-To", "").replace("http://", "").replace("https://", "") or None
     callback = req.headers.get("X-Callback-Url")
@@ -33,20 +37,19 @@ async def forward_request(req: Request, path: str):
 
                 if callback:
                     print("Callback, sending to", callback)
-                    async for chunk in resp.content.iter_chunked(4096):
-                        async with aiohttp.ClientSession() as session:
-                          await session.request(
-                              "POST",
-                              callback,
-                              data=chunk,
-                              headers=returned_headers
-                          )
+                    data = chunked_response(resp)
+                    async with aiohttp.ClientSession() as session:
+                        callback_resp = await session.post(
+                            callback,
+                            data=data,
+                            headers=returned_headers
+                        )
+                        print("Got response", callback_resp.status, callback_resp.headers)
                     print("Done sending data, terminating")
                     return HTTPResponse(body="OK", status=200)
                 else:
                     print("No callback, sending back data")
                     return HTTPResponse(body=await resp.read(), headers=returned_headers, status=returned_status)
-
 
 @app.route("/<path:path>", methods=["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"])
 async def index(req: Request, path: str):
@@ -54,7 +57,6 @@ async def index(req: Request, path: str):
       asyncio.create_task(forward_request(req, path))
       return HTTPResponse(body="CREATED", status=201)
     return await forward_request(req, path)
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(getenv("PORT", 8000)), debug=True)
